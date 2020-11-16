a6c1fc9700e395eb9faef9bf349c6427d0c55d4b
SentientSam <samueltlamb1997@gmail.com>
2020-11-16 17:57:38 -0600

    HelloWorld

---
+++ HelloWorld.py
@@ -0,0 +1,18 @@
+import tornado.ioloop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello World!")
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(3000)
+    tornado.ioloop.IOLoop.current().start()
+# To see the "Hello World!" go to your browser and type in localhost:3000 in the url bar at the top
+    
